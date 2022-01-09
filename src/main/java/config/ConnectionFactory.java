8bea3bd97479d6ceeb29ae5ddf5bd5306fcb06a4
Meisam <zsmeimad@gmail.com>
2022-01-10 00:27:10 +0400

    create database if not exist

--- src/main/java/config/ConnectionFactory.java
+++ src/main/java/config/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public static final String URL = "jdbc:mysql://localhost:3306/test_db";
+    public static final String URL = "jdbc:mysql://localhost:3306/test_db?createDatabaseIfNotExist=true";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "root";
 
